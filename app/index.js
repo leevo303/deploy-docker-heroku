181556ae4ea65af98cc5b0a15898061039019a25
leevo <leevo.303@gmail.com>
2022-06-24 02:16:06 +0700

    Add heroku config for auto deploy

--- app/index.js
+++ app/index.js
@@ -4,4 +4,4 @@ const PORT = process.env.PORT || 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`,process.env.PORT.TEST ))
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`,process.env.TEST ))
